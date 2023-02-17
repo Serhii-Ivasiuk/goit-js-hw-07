3a7537962496586045dadd61798fcaa30b94fadf
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-02-17 21:42:24 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,9 +9,11 @@ function createGalleryItemsMarkpup(items) {
 	return items
 		.map(
 			({ original, preview, description }) => `
-				  <a class="gallery__item" href="${original}">
-				    <img class="gallery__image" src="${preview}" alt="${description}" />
-				  </a>
+				  <li>
+						<a class="gallery__item" href="${original}">
+				    	<img class="gallery__image" src="${preview}" alt="${description}" />
+				  	</a>
+					</li>
 				`
 		)
 		.join('');
