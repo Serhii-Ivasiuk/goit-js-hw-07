d74c06b10d16aaad91ece571855fdb25b98d707b
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-02-17 21:39:14 +0200

    SimpleLightbox plugin

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkpup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemsMarkpup(items) {
+	return items
+		.map(
+			({ original, preview, description }) => `
+				  <a class="gallery__item" href="${original}">
+				    <img class="gallery__image" src="${preview}" alt="${description}" />
+				  </a>
+				`
+		)
+		.join('');
+}
+
+new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
